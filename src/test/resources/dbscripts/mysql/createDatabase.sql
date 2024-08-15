e742c8f2b5e542049591d615266340c5c9d04fa1
Niu Liu <liuniu@tsinghua.org.cn>
2024-08-16 09:40:05 +1200

    minor adjust on createDatabase.sql

--- src/test/resources/dbscripts/mysql/createDatabase.sql
+++ src/test/resources/dbscripts/mysql/createDatabase.sql
@@ -2,3 +2,4 @@
 create database testenv;
 grant all privileges on testenv.* to 'testenv'@'%' identified by 'abcd1234';
 grant all privileges on testenv.* to 'testenv'@'localhost' identified by 'abcd1234';
+flush privileges;
